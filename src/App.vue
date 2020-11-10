e7c6e9bcf1c863d982e4ad44e775b2573964f4ff
Yimning <1148967988@qq.com>
2020-11-10 10:19:32 +0800

    App.vue主组件

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+        <router-view v-if="isRouterAlive"></router-view>
+    </div>
+</template>
+<style>
+@import './assets/css/main.css';
+/* 深色主题  */
+@import './assets/css/color-dark.css';
+/*@import "./assets/css/theme-green/color-green.css";   浅绿色主题*/
+</style> 
+<script>
+export default { 
+    name: 'app',
+    provide() {
+        return {
+            reload: this.reload
+        };
+    },
+    data() {
+        return {
+            isRouterAlive: true
+        };
+    },
+    methods: {
+        reload() {
+            this.isRouterAlive = false;
+            this.$nextTick(function () {
+                this.isRouterAlive = true;
+            });
+        }
+    }
+};
+</script>
