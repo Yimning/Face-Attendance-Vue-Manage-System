37d5d7623f0a1a1c63a49df338b3c0e829433c02
Yimning <1148967988@qq.com>
2020-11-17 16:49:37 +0800

    更新

--- src/views/Donate.vue
+++ src/views/Donate.vue
@@ -8,29 +8,37 @@
             </el-breadcrumb>
         </div>
         <div class="container">
+            <div align="center">
+                <p id="flag" class="tishi"></p>
+            </div>
+
             <!-- <div class="getface">
                 <img id="imgTag" src alt="imgTag" style="display: none;" />
             </div>-->
-            <el-button type="primary" icon="el-icon-camera-solid" @click="openCamera">打开摄像头</el-button>
-            <el-button type="primary" icon="el-icon-camera-solid" @click="closeCamera">关闭摄像头</el-button>
-          
-            <!--提示-->
-            <div align="center">
-                <p id="flag" class="tishi"> </p>
-            </div>
+            <el-button type="primary" icon="el-icon-camera-solid" @click="openCamera">点击考勤</el-button>
+            <!-- <el-button type="primary" icon="el-icon-camera-solid" @click="closeCamera">关闭</el-button> -->
 
-            <div align="center">
+            <div>
                 <!--开启摄像头-->
-                <video id="video" ref="video" width="480px" height="400px" autoplay="autoplay"></video>
+                <!-- <img @click="openCamera" :src="headImgSrc" alt="摄像头" /> -->
+                <video ref="video" width="480px" height="400px" autoplay="autoplay"></video>
                 <!-- <video id="video" preload autoplay loop muted></video> -->
                 <!--canvas截取流-->
+                <!-- <canvas ref="canvas" width="480px" height="400px" style="display: none;"></canvas> -->
                 <canvas
                     ref="canvas"
                     id="canvas"
-                    width="480px"
-                    height="400px"
-                    style="display: none;"
+                    width="500"
+                    height="500"
+                    style="position:absolute;top:0;left:0;"
                 ></canvas>
+
+                <!-- <div class="getface">
+                    <img id="imgTag" src alt="imgTag" style="display: none;" />
+                </div> -->
+                <!--图片展示-->
+                <!--确认-->
+                <!-- <el-button size="mini" type="primary" @click="photograph"></el-button> -->
             </div>
         </div>
     </div>
@@ -51,18 +59,18 @@ export default {
             first: null
         };
     },
-
+    created() {
+        setTimeout(() => {
+            this.openCamera(); // 此为绘画canvas的方法调用
+        }, 2);
+    },
+    mounted() {},
     methods: {
-        created() {
-            setTimeout(() => {
-                this.openCamera(); // 此为绘画canvas的方法调用
-            }, 2);
-        },
-        mounted() {},
         openCamera() {
             var video = document.getElementById('video');
             var canvas = document.getElementById('canvas');
             var context = this.$refs['canvas'].getContext('2d');
+
             var tracker = new tracking.ObjectTracker('face'); // 引入第三方库  cnpm install tracking --save
             tracker.setInitialScale(1);
             tracker.setStepSize(2);
@@ -92,87 +100,52 @@ export default {
         tracker_fun(tracker, video, context, canvas) {
             let that = this;
             let set_clear;
-            let flag = true;
             set_clear = setTimeout(function () {
                 // 每秒 检测人脸 结果
                 tracker.on('track', function (event) {
                     // 视频流
-                    context.clearRect(0, 0, canvas.width, canvas.height);
-                    event.data.forEach(function (rect) {
-                        context.strokeStyle = '#ff0000';
-                        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-                    });
-                    if (event.data.length) {
-                        // 会不停的去检测人脸，所以这里需要做个锁
-                        if (flag) {
-                            // 裁剪出人脸并绘制下来
-                            const canvasUpload = document.getElementById('canvas');
-                            const contextUpload = canvasUpload.getContext('2d');
-                            contextUpload.drawImage(video, 0, 0, 105, 105);
-                            flag = false;
-                            // 人脸的basa64
-                            const dataURL = canvasUpload.toDataURL('image/jpeg');
-                            console.log(dataURL);
-                            // // ajax请求
-                            // _this.$store
-                            //     .dispatch('LoginByFaceID', {
-                            //         face_img: dataURL.slice(23) // 我们后台需要的basa64不要前缀
-                            //     })
-                            //     .then((res) => {
-                            //         let type = 'success';
-                            //         // 登录成功跳转到首页
-                            //         if (res.data.code === 200) {
-                            //             _this.$router.push({
-                            //                 path: '/'
-                            //             });
-                            //         }
-                            //         // 登录失败重新进行人脸检测
-                            //         else {
-                            //             type = 'error';
-                            //             setTimeout(() => {
-                            //                 flag = true;
-                            //             }, 1500);
-                            //         }
-                            //         _this.$message({
-                            //             message: res.data.message,
-                            //             type,
-                            //             center: true
-                            //         });
-                            //     })
-                            //     .catch((error) => {
-                            //         console.log(error);
-                            //     });
-                            video.load();
-                            that.first = null;
-                            that.tracker_fun(tracker, video, context, canvas);
-                        }
-                    } else {
-                        console.log('没有检测到人脸');
+                    // context.clearRect(0, 0, canvas.width, canvas.height);
+                    if (!that.first) {
+                        // if  --- > else  检测到人脸 success() =>{}
+                        event.data.forEach(function (rect) {
+                            if (rect.x) {
+                                that.first = rect.x;
+                                video.pause(); // success  将暂停 video
+                                console.log(rect);
+                                console.log(video);
+                                context.drawImage(video, 0, 0, 500, 500); // 绘制到 canvas
+                                context.font = '11px Helvetica';
+                                context.fillText('', 100, 40);
+                                context.strokeStyle = '#a64ceb';
+                                context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+                                var data_url = canvas.toDataURL('image/png'); //base64 request
+                               // console.log(data_url);
+
+                               setTimeout(() => {
+                                    //设置延迟执行
+                                     video.load();
+                                   console.log("---------------------"+data_url);
+                                }, 5000);
+                                //video.load();
+                                that.first = null;
+                                that.tracker_fun(tracker, video, context, canvas);
+                                return;
+                            }
+                        });
                     }
                 });
                 clearTimeout(set_clear);
-                console.log('5秒重新加载摄像头');
+                console.log('-------');
             }, 5000);
-        },
-        // 关闭摄像头
-        closeCamera() {
-            if (!this.$refs['video'].srcObject) return;
-            let stream = this.$refs['video'].srcObject;
-            let tracks = stream.getTracks();
-            tracks.forEach((track) => {
-                track.stop();
-            });
-            this.$refs['video'].srcObject = null;
         }
+    },
+    mounted() {},
+    destroyed() {
+        // 停止侦测
+        this.trackerTask.stop();
+        // 关闭摄像头
+        window.tracking.closeCamera();
     }
-    // mounted() {},
-    // destroyed() {
-    //     // 停止侦测
-    //     this.trackerTask.stop();
-    //     // 关闭摄像头
-    //     window.tracking.closeCamera();
-
-    // }
 };
 </script>
 
