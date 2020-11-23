0ec98f13f4213b61eadeb92ef69d3e24800a2a12
Yimning <1148967988@qq.com>
2020-11-23 12:44:18 +0800

    更新

--- src/views/SignIn/StuAttendenceRecords.vue
+++ src/views/SignIn/StuAttendenceRecords.vue
@@ -196,7 +196,7 @@ import JsonExcel from 'vue-json-excel';
 const weekArr = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
 export default {
     name: 'basetable',
-    inject: ['reload'],
+    inject: ['reload'], 
     data() {
         return {
             query: {
