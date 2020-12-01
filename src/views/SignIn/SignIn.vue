ab29f17f61701ebbb0edd848352e47c32deb8356
Yimning <1148967988@qq.com>
2020-12-01 18:56:03 +0800

    更新

--- src/views/SignIn/SignIn.vue
+++ src/views/SignIn/SignIn.vue
@@ -46,7 +46,7 @@ export default {
                 courseID:'',
                 UserID:'',
             },
-            dataParams: {},
+            dataParams: {},   
         };
     },  
     created() {
