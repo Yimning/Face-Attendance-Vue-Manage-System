f9de0cbbb04a67a6a842945a4d1a2a804006407e
Yimning <1148967988@qq.com>
2020-12-02 11:24:11 +0800

    添加组件的倒计时功能

--- src/views/SignIn/SignIn.vue
+++ src/views/SignIn/SignIn.vue
@@ -24,6 +24,10 @@
                 <!--canvas截取流-->
                 <canvas ref="canvas" id="canvas" width="480px" height="400px" style="display: none"></canvas>
             </div>
+            <div class="time">
+                <p>00:{{ timerCount2 }}:{{ timerCount1 }}</p>
+                <button @click="reset">重新计时</button>
+            </div>
         </div>
     </div>
 </template>
@@ -43,16 +47,40 @@ export default {
             first: null,
             faceInfo: {
                 imgpath: '',
-                courseID:'',
-                UserID:'',
+                courseID: '',
+                UserID: ''
             },
-            dataParams: {},   
+            dataParams: {},
+            timeSeconds: 0,
+            timeMinutes: 0,
+            seconds: 59, // 秒
+            minutes: 1, // 分
+            timer: null
         };
-    },  
+    },
     created() {
-        this.dataParams=this.$route.query.data; 
-        this.faceInfo.courseID=this.dataParams.courseID;
-        console.log( this.faceInfo);
+        this.dataParams = this.$route.query.data;
+        this.faceInfo.courseID = this.dataParams.courseID;
+        console.log(this.faceInfo);
+    },
+    mounted() {
+        if (sessionStorage.getItem('answered') != 1) {
+            this.timing();
+        }
+    },
+    computed: {
+        timerCount1() {
+            return this.timeSeconds < 10 ? '0' + this.timeSeconds : '' + this.timeSeconds;
+        },
+        timerCount2() {
+            return this.timeMinutes < 10 ? '0' + this.timeMinutes : '' + this.timeMinutes;
+        }
+    },
+    destroyed() {
+        // 退出后清除计时器
+        if (this.timer) {
+            clearInterval(this.timer);
+        }
     },
     methods: {
         created() {
@@ -136,7 +164,7 @@ export default {
                                     if (res.data.error_code === 0 && res.data.face_liveness > 0.8) {
                                         that.closeCamera();
                                         // 登录成功跳转到首页
-                                       // that.$router.push('/dashboard');
+                                        // that.$router.push('/dashboard');
                                         that.$message.success('登录成功');
                                         that.FaceLoginVisible = false;
                                     } else {
@@ -172,6 +200,57 @@ export default {
                 track.stop();
             });
             this.$refs['video'].srcObject = null;
+        },
+        num(n) {
+            return n < 10 ? '0' + n : '' + n;
+        },
+        // 重新计时
+        reset() {
+            sessionStorage.removeItem('answered');
+            window.location.reload();
+            localStorage.removeItem('startTime1');
+            localStorage.removeItem('startTime2');
+            clearInterval(this.timer);
+        },
+        // 清除
+        clear() {
+            localStorage.removeItem('startTime1');
+            localStorage.removeItem('startTime2');
+            sessionStorage.setItem('answered', 1);
+            clearInterval(this.timer);
+        },
+        // 倒计时
+        timing() {
+            let [startTime1, startTime2] = [localStorage.getItem('startTime1'), localStorage.getItem('startTime2')];
+            let nowTime = new Date().getTime();
+            if (startTime1) {
+                let surplus = this.seconds - parseInt((nowTime - startTime1) / 1000);
+                this.timeSeconds = surplus <= 0 ? 0 : surplus;
+            } else {
+                this.timeSeconds = this.seconds;
+                localStorage.setItem('startTime1', nowTime); //存储秒
+            }
+            if (startTime2) {
+                this.timeMinutes = startTime2;
+            } else {
+                this.timeMinutes = this.minutes;
+                localStorage.setItem('startTime2', this.minutes); //存储分
+            }
+            this.timer = setInterval(() => {
+                if (this.timeSeconds == 0 && this.timeMinutes != 0 && this.timeMinutes > 0) {
+                    let nowTime = new Date().getTime();
+                    this.timeSeconds = this.seconds;
+                    localStorage.setItem('startTime1', nowTime);
+                    this.timeMinutes--;
+                    localStorage.setItem('startTime2', this.timeMinutes);
+                } else if (this.timeMinutes == 0 && this.timeSeconds == 0) {
+                    this.timeSeconds = 0;
+                    this.clear();
+                    alert('考试时间到');
+                } else {
+                    this.timeSeconds--;
+                }
+            }, 1000);
         }
     }
     // mounted() {},
